8c403a5a6296969ea5a6d3fd0cea1cc65795197b
Colin Alworth <colin@colinalworth.com>
2021-02-25 15:37:39 -0600

    Switch to j2cl, make sure it still works

--- my-button-gwt-example/src/main/java/com/github/lofi/client/AppEntryPoint.java
+++ my-button-gwt-example/src/main/java/com/github/lofi/client/AppEntryPoint.java
@@ -2,13 +2,13 @@ package com.github.lofi.client;
 
 import com.github.lofi.client.components.Es5CustomElementHelper;
 import com.github.lofi.client.components.MyButton;
-import com.google.gwt.core.client.EntryPoint;
+//import com.google.gwt.core.client.EntryPoint;
 
 import elemental2.dom.DomGlobal;
 
-public class AppEntryPoint implements EntryPoint {
+public class AppEntryPoint /*implements EntryPoint*/ {
 
-	@Override
+//	@Override
 	public void onModuleLoad() {
 //		DomGlobal.customElements.define("my-button", MyButton.class);
 		Es5CustomElementHelper.registerEs5("my-button", MyButton.class, MyButton::init);
